ef804f854f5e14f7495faddb125cae93e46ccbdd
Luke Dixon <luke@luke.dixon.name>
2018-12-09 11:12:34 +0000

    2018 day 2: Remove some rubbish from the top
    I think that might actually have been day one.

--- 2018/02/day2.py
+++ 2018/02/day2.py
@@ -1,49 +1,3 @@
-with open('input2.txt') as f:
-    data = f.read()
-
-
-total = 0
-for d in data.split():
-  if d.startswith('+'):
-    total += int(d[1:])
-  if d.startswith('-'):
-    total -= int(d[1:])
-
-print(total)
-
-
-
-
-total = 0
-frequencies = set()
-frequencies.add(total)
-
-found = False
-repeats = -1
-while not found:
-  repeats += 1
-  for d in data.split():
-    if d.startswith('+'):
-      total += int(d[1:])
-    if d.startswith('-'):
-      total -= int(d[1:])
-    if d == '':
-      print('breaking')
-      break
-    if total == 502:
-      print('502 found', d)
-    if total in frequencies:
-      print('Duplicate', total)
-      found = True
-      break
-    frequencies.add(total)
-
-print('Total', total)
-print('Repeats', repeats)
-
-
-
-
 from collections import Counter
 with open('input2.txt') as f:
   data = f.read()
@@ -71,7 +25,6 @@ for i in range(len(sorted_data)):
     diff_index = 0
     for n in range(len(sorted_data[i])):
       if sorted_data[i][n] != sorted_data[j][n]:
-        #print('d[n] != x[n]', d[n], x[n])
         diff += 1
         diff_index = n
     if diff == 1:
