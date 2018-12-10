0b6487e8d92ecdd302c6ba2141241853cffc3193
Luke Dixon <luke@luke.dixon.name>
2018-12-10 20:22:09 +0000

    2017 day13: check scanners without running them
    This changes part two to not bother updating each scanner to where they
    need to be and just calculates what we need to check. This made it
    clearer for me to see where I could put in a break to speed things up.
    Makes things way faster than they were.
    
    This also changes the Scanner class to be the one I originally used from
    part 2.

--- 2017/13/day13.py
+++ 2017/13/day13.py
@@ -4,52 +4,6 @@ with open('input13.txt') as f:
 
 class Scanner:
   def __init__(self, range):
-    self.range = range
-    self.current = 0
-    self.direction = 0
-
-  def update(self, steps=1):
-    for _ in range(steps):
-      if self.direction == 0 and self.current < self.range - 1:
-        self.current += 1
-      elif self.direction == 0 and self.current == self.range - 1:
-        self.current -= 1
-        self.direction = 1
-      elif self.direction == 1 and self.current > 0:
-        self.current -= 1
-      elif self.direction == 1 and self.current == 0:
-        self.current += 1
-        self.direction = 0
-
-  def __repr__(self):
-    return f'Scanner(current={self.current}, range={self.range})'
-
-
-scanners = {}
-for d in data:
-  scanners[d[0]] = Scanner(d[1])
-
-
-def run(player_initial=-1):
-  caught = 0
-  player = -1
-
-  while player < max(scanners.keys()):
-    player += 1
-    if player in scanners and scanners[player].current == 0:
-      caught += player * scanners[player].range
-
-    for scanner in scanners:
-      scanners[scanner].update()
-
-  return caught
-
-print(f'Day 13 Answer 1 {run()}')
-
-
-# First scanner was too slow, need a new one
-class Scanner2:
-  def __init__(self, range):
     self.range = (2 * range) - 2
     self.current = 0
 
@@ -63,28 +17,30 @@ class Scanner2:
 
 scanners = {}
 for d in data:
-  scanners[d[0]] = Scanner2(d[1])
+  scanners[d[0]] = Scanner(d[1])
 
 
-i = 0
+def run(player_initial=-1):
+  caught = 0
+  player = -1
+  while player < max(scanners.keys()):
+    player += 1
+    if player in scanners and scanners[player].current == 0:
+      caught += player * scanners[player].range
+    for scanner in scanners:
+      scanners[scanner].update()
+  return caught
+
+print(f'Day 13 Answer 1 {run()}')
+
+i = -1
 found = False
 while not found:
-  # Reset all scanners
-  for scanner in scanners:
-    scanners[scanner].current = 0
-
-  # Run scanners to their current configuration for this run
-  for scanner in scanners:
-    scanners[scanner].update(steps=scanner + i)
-
-  # Check we don't get caught
+  i += 1
   found = True
   for scanner in scanners:
-    if scanners[scanner].current == 0:
+    if (i + scanner) % scanners[scanner].range == 0:
       found = False
       break
-
-  # Another go
-  i += 1
 
 print(f'Day 13 Answer 2 {i - 1}')
