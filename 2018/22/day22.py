b8126a49489fc6eb3f03e700c76dfc448f20b4fd
Luke Dixon <luke@luke.dixon.name>
2019-04-14 20:56:35 +0100

    2018 22: clean up code a bit
    Here I've limited the size of the grid created instead of the graph,
    since that limits the amount of memory taken up early on.
    I've also moved plenty around and changed from a queue to a stack when
    creating the graph, which seemed slightly faster.

--- 2018/22/day22.py
+++ 2018/22/day22.py
@@ -1,14 +1,6 @@
-from collections import deque, namedtuple
-
 import networkx as nx
 
 
-def draw_grid(grid):
-    for row in grid:
-        print(''.join(row))
-
-
-CellData = namedtuple('CellData', ['geologic_index', 'erosion_level', 'torch_visited', 'climbing_gear_visited', 'unequiped_visited'])
 possible_moves = {
     'torch': ('.', '|'),
     'climbing gear': ('.', '='),
@@ -16,87 +8,12 @@ possible_moves = {
 }
 
 
-def main():
-    depth, target = get_data()
-    print(target)
-    grid, grid_data = construct_grid(depth, target)
-
-    #draw_grid(grid)
-    print(f'Day 22 Part 1 Answer: {calculate_risk_level(grid_data, target)}')
-
-    print(f'length of grid-data {len(grid_data)}')
-
-    print('Constructing graph')
-    nodes_in_queue = set()
-    nodes_processed = set()
-    queue = deque()
-    graph = nx.Graph()
-    graph.add_node((0, 0, 'torch'))
-    queue.append((0, 0, 'torch'))
-    nodes_in_queue.add((0, 0, 'torch'))
-    i = 0
-    while len(queue):
-        i += 1
-        if i % 2500000 == 0 and (0, 0, 'torch') in graph.nodes and (target[0], target[1], 'torch') in graph.nodes:
-            print(f'Graph Nodes: {len(graph.nodes)}')
-            print(f'Graph Edges: {len(graph.edges)}')
-            try:
-                shortest_path = nx.algorithms.shortest_path(graph, source=(0, 0, 'torch'), target=(target[0], target[1], 'torch'))
-                weight = 0
-                for edge in zip(shortest_path[:-1], shortest_path[1:]):
-                    weight += graph.edges[edge]['weight']
-                print(f'shortest path length: {len(shortest_path)} {weight}')
-            except Exception:
-                pass
-        y, x, tool = queue.popleft()
-        #y, x, tool = queue.pop()
-        if i % 100000 == 0:
-            print(i, (x, y, tool))
-
-        nodes_in_queue.remove((y, x, tool))
-        nodes_processed.add((y, x, tool))
-
-        for next_tool in ['torch', 'climbing gear', 'neither']:
-            if next_tool == tool:
-                continue
-            if grid[y][x] not in possible_moves[next_tool]:
-                continue
-            graph.add_edge((y, x, tool), (y, x, next_tool), weight=7)
-            if not ((y, x, next_tool) in nodes_in_queue or (y, x, next_tool) in nodes_processed):
-                queue.append((y, x, next_tool))
-                nodes_in_queue.add((y, x, next_tool))
-
-        search_add_edge_and_queue(graph, grid, nodes_in_queue, nodes_processed, queue, y, x, tool, y + 1, x, target)
-        search_add_edge_and_queue(graph, grid, nodes_in_queue, nodes_processed, queue, y, x, tool, y - 1, x, target)
-        search_add_edge_and_queue(graph, grid, nodes_in_queue, nodes_processed, queue, y, x, tool, y, x + 1, target)
-        search_add_edge_and_queue(graph, grid, nodes_in_queue, nodes_processed, queue, y, x, tool, y, x - 1, target)
-
-    print(f'Graph Nodes: {len(graph.nodes)}')
-    print(f'Graph Edges: {len(graph.edges)}')
-    shortest_path = nx.algorithms.dijkstra_path(graph, source=(0, 0, 'torch'), target=(target[0], target[1], 'torch'))
-    weight = 0
-    for edge in zip(shortest_path[:-1], shortest_path[1:]):
-        weight += graph.edges[edge]['weight']
-    print(shortest_path)
-    print(f'shortest path length: {len(shortest_path)} {weight}')
-
-
-def search_add_edge_and_queue(graph, grid, nodes_in_queue, nodes_processed, queue, y, x, tool, new_y, new_x, target):
-    if new_y < 0 or new_x < 0:
-        return
-
-    # Guess it's kind of not going to be way out
-    if new_y > target[0] + 20 or new_x > target[1] + 20:
-        return
-
-    try:
-        if grid[new_y][new_x] in possible_moves[tool]:
-            graph.add_edge((y, x, tool), (new_y, new_x, tool), weight=1)
-            if not ((new_y, new_x, tool) in nodes_in_queue or (new_y, new_x, tool) in nodes_processed):
-                queue.append((new_y, new_x, tool))
-                nodes_in_queue.add((new_y, new_x, tool))
-    except IndexError:
-        pass
+def get_data():
+    with open('input22.txt') as f:
+        depth = int(f.readline().strip('\n').lstrip('depth: '))
+        target = f.readline().strip('\n').lstrip('target: ').split(',')
+    target = (int(target[1]), int(target[0]))
+    return depth, target
 
 
 def calculate_risk_level(grid_data, target):
@@ -110,9 +27,10 @@ def calculate_risk_level(grid_data, target):
 
 def construct_grid(depth, target):
     grid = []
-    for y in range(depth):
+    beyond_target_limit = 20  # TODO: how much is safe?
+    for y in range(min(depth, target[0] + beyond_target_limit)):
         row = []
-        for x in range(depth):
+        for x in range(min(depth, target[1] + beyond_target_limit)):
             row.append('.')
         grid.append(row)
     grid_data = {}
@@ -151,12 +69,70 @@ def construct_grid(depth, target):
     return grid, grid_data
 
 
-def get_data():
-    with open('input22.txt') as f:
-        depth = int(f.readline().strip('\n').lstrip('depth: '))
-        target = f.readline().strip('\n').lstrip('target: ').split(',')
-    target = (int(target[1]), int(target[0]))
-    return depth, target
+def graph_add_edges_to_tool_changes(graph, grid, nodes_processed, stack, tool, y, x):
+    for next_tool in ['torch', 'climbing gear', 'neither']:
+        if next_tool == tool:
+            continue
+        if grid[y][x] not in possible_moves[next_tool]:
+            continue
+        graph.add_edge((y, x, tool), (y, x, next_tool), weight=7)
+        if (y, x, next_tool) not in nodes_processed:
+            stack.append((y, x, next_tool))
+            nodes_processed.add((y, x, next_tool))
+
+
+def graph_add_edges_to_adjacent(graph, grid, nodes_processed, stack, y, x, tool, new_y, new_x):
+    if new_y < 0 or new_x < 0:
+        return
+
+    try:
+        if grid[new_y][new_x] in possible_moves[tool]:
+            graph.add_edge((y, x, tool), (new_y, new_x, tool), weight=1)
+            if (new_y, new_x, tool) not in nodes_processed:
+                stack.append((new_y, new_x, tool))
+                nodes_processed.add((new_y, new_x, tool))
+    except IndexError:
+        pass
+
+
+def graph_from_grid(grid):
+    nodes_processed = set()
+    stack = []
+    graph = nx.Graph()
+    graph.add_node((0, 0, 'torch'))
+    stack.append((0, 0, 'torch'))
+    nodes_processed.add((0, 0, 'torch'))
+    while len(stack):
+        y, x, tool = stack.pop()
+
+        graph_add_edges_to_tool_changes(graph, grid, nodes_processed, stack, tool, y, x)
+
+        graph_add_edges_to_adjacent(graph, grid, nodes_processed, stack, y, x, tool, y + 1, x)
+        graph_add_edges_to_adjacent(graph, grid, nodes_processed, stack, y, x, tool, y - 1, x)
+        graph_add_edges_to_adjacent(graph, grid, nodes_processed, stack, y, x, tool, y, x + 1)
+        graph_add_edges_to_adjacent(graph, grid, nodes_processed, stack, y, x, tool, y, x - 1)
+    return graph
+
+
+def part1(grid_data, target):
+    print(f'Day 22 Part 1 Answer: {calculate_risk_level(grid_data, target)}')
+
+
+def part2(grid, target):
+    graph = graph_from_grid(grid)
+    shortest_path = nx.algorithms.dijkstra_path(graph, source=(0, 0, 'torch'), target=(target[0], target[1], 'torch'))
+    weight = 0
+    for edge in zip(shortest_path[:-1], shortest_path[1:]):
+        weight += graph.edges[edge]['weight']
+    print(f'Day 22 Part 2 Answer: {weight}')
+
+
+def main():
+    depth, target = get_data()
+    grid, grid_data = construct_grid(depth, target)
+
+    part1(grid_data, target)
+    part2(grid, target)
 
 
 if __name__ == "__main__":
