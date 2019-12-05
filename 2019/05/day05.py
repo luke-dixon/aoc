95d1fffb57661c520d03e3fccfce935382abc563
Luke Dixon <luke@luke.dixon.name>
2019-12-05 21:00:37 +0000

    2019 day 5: More intcode computer

---
+++ 2019/05/day05.py
@@ -0,0 +1,160 @@
+from typing import List, Callable, Dict
+
+from aocd.models import Puzzle
+
+
+class Halt(Exception):
+    pass
+
+
+class Parameter:
+    def __init__(self, data, position, mode):
+        self.data = data
+        self.position = position
+        self.mode = mode
+
+    def get(self):
+        if self.mode:
+            return self.data[self.position]
+        else:
+            return self.data[self.data[self.position]]
+
+    def set(self, value):
+        if self.mode:
+            self.data[self.position] = value
+        else:
+            self.data[self.data[self.position]] = value
+
+
+class Instruction:
+    def __init__(self, data, position, operation, input_, output, parameter1, parameter2, parameter3):
+        self.data = data
+        self.position = position
+        self.operation = operation
+        self.input = input_
+        self.output = output
+        self.parameter1 = parameter1
+        self.parameter2 = parameter2
+        self.parameter3 = parameter3
+
+    def __call__(self):
+        return self.operation(self)
+
+
+def add(self: Instruction) -> int:
+    self.parameter3.set(self.parameter1.get() + self.parameter2.get())
+    return self.position + 4
+
+
+def multiply(self: Instruction) -> int:
+    self.parameter3.set(self.parameter1.get() * self.parameter2.get())
+    return self.position + 4
+
+
+def halt(self: Instruction) -> int:
+    raise Halt
+
+
+def store(self: Instruction) -> int:
+    value = self.input.pop(0)
+    self.parameter1.set(value)
+    return self.position + 2
+
+
+def retrieve(self: Instruction) -> int:
+    self.output.append(self.parameter1.get())
+    return self.position + 2
+
+
+def jump_if_true(self: Instruction) -> int:
+    if self.parameter1.get() != 0:
+        return self.parameter2.get()
+    else:
+        return self.position + 3
+
+
+def jump_if_false(self: Instruction) -> int:
+    if self.parameter1.get() == 0:
+        return self.parameter2.get()
+    else:
+        return self.position + 3
+
+
+def less_than(self: Instruction) -> int:
+    if self.parameter1.get() < self.parameter2.get():
+        self.parameter3.set(1)
+    else:
+        self.parameter3.set(0)
+    return self.position + 4
+
+
+def equals(self: Instruction) -> int:
+    if self.parameter1.get() == self.parameter2.get():
+        self.parameter3.set(1)
+    else:
+        self.parameter3.set(0)
+    return self.position + 4
+
+
+operations: Dict[int, Callable[[Instruction], int]] = {
+    1: add,
+    2: multiply,
+    3: store,
+    4: retrieve,
+    5: jump_if_true,
+    6: jump_if_false,
+    7: less_than,
+    8: equals,
+    99: halt,
+}
+
+
+def instruction_factory(data, position, input_, output):
+    opcode = str(data[position]).rjust(5, '0')
+    return Instruction(
+        data,
+        position,
+        operations[int(opcode[3:])],
+        input_,
+        output,
+        Parameter(data, position + 1, int(opcode[2])),
+        Parameter(data, position + 2, int(opcode[1])),
+        Parameter(data, position + 3, int(opcode[0])),
+    )
+
+
+def run_intcode_computer(program, input_):
+    output = []
+    position = 0
+    while True:
+        instruction = instruction_factory(program, position, input_, output)
+        try:
+            position = instruction()
+        except Halt:
+            break
+    return output
+
+
+class Day5(Puzzle):
+    def __init__(self):
+        super().__init__(year=2019, day=5)
+        self.data = tuple([int(x) for x in self.input_data.split(',')])
+
+    def part1(self):
+        output = run_intcode_computer(list(self.data), [1])
+        return output[-1]
+
+    def part2(self):
+        output = run_intcode_computer(list(self.data), [5])
+        return output[-1]
+
+
+def main():
+    puzzle = Day5()
+
+    print(f'Part 1 Answer: {puzzle.part1()}')
+    print(f'Part 2 Answer: {puzzle.part2()}')
+
+
+if __name__ == '__main__':
+    main()
